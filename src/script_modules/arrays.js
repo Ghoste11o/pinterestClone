74526c52fcebfc9beab5240341c87cf7c4495ba2
Romancheg <ghoste11o@yandex.ru>
2023-04-13 05:08:41 +0300

    pintclone_done

---
+++ src/script_modules/arrays.js
@@ -0,0 +1,41 @@
+// boards list
+const arrBoards = [
+    {
+        tag: "Board-1",
+        id: "brd1",
+    },
+    {
+        tag: "Board-2",
+        id: "brd2",
+    },
+    {
+        tag: "Board-3",
+        id: "brd3",
+    },
+];
+
+// problems list
+const arrProblems = [
+    {
+        tag: "Problem-1",
+        check: false,
+    },
+    {
+        tag: "Problem-2",
+        check: false,
+    },
+    {
+        tag: "Problem-3",
+        check: false,
+    },
+    {
+        tag: "Problem-4",
+        check: false,
+    },
+    {
+        tag: "Problem-5",
+        check: false,
+    },
+];
+
+export {arrBoards, arrProblems};
--- src/scripts.js
+++ src/scripts.js
@@ -1,72 +1,20 @@
 "use strict";
 
-// arrays for save to LS (optional)
-const arrBoards = [
-    {
-        tag: "Board-1",
-        id: 1,
-    },
-    {
-        tag: "Board-2",
-        id: 2,
-    },
-    {
-        tag: "Board-3",
-        id: 3,
-    },
-];
+import {createElemWithID, createElemWithClass} from "./script_modules/utils.js"
 
-const arrProblems = [
-    {
-        tag: "Problem-1",
-        check: false,
-    },
-    {
-        tag: "Problem-2",
-        check: false,
-    },
-    {
-        tag: "Problem-3",
-        check: false,
-    },
-    {
-        tag: "Problem-4",
-        check: false,
-    },
-    {
-        tag: "Problem-5",
-        check: false,
-    },
-];
-
-const boardOne = [];
-const boardTwo = [];
-const boardThree = [];
+import {arrBoards, arrProblems} from "./script_modules/arrays.js"
 
 // global variables
-const allData = JSON.parse(localStorage.getItem("data"));
+let allData;
+let filterData;
+
+const URL = "https://63fd39438ef914c555a6fbb7.mockapi.io/api/v1/data";
 const btnSearch = document.getElementById("headerBtnSearch");
 const input = document.getElementById("headerInput");
 const mainList = document.getElementById("mainList");
 const btnBoards = document.getElementById("headerBtnBoards");
 const boards = document.getElementById("headerBoards");
-const overlay = document.getElementById("modalWindow");
-
-// new elem with ID
-const createElemWithID = (tag, idDescr, descr) => {
-    let elem = document.createElement(tag);
-    elem.setAttribute("id", idDescr);
-    (!!descr) ? elem.append(descr) : elem;
-    return elem;
-};
-
-// new input with ID and Type
-const createInputIdType = (idDescr, inputType) => {
-    let elem = document.createElement("input");
-    elem.setAttribute("id", idDescr);
-    elem.setAttribute("type", inputType);
-    return elem;
-};
+const overlayWindow = document.getElementById("modalWindow");
 
 // dropdown button Boards
 boards.addEventListener("mouseover", () => boards.style.display = "block");
@@ -75,43 +23,35 @@ boards.addEventListener("mouseout", () => boards.style.display = "none");
 btnBoards.addEventListener("click", () => {
     boards.innerHTML = "";
 
+    // toggle dropdown menu
     boards.style.display == "block" ? boards.style.display = "none" : boards.style.display = "block";
 
-    arrBoards.forEach(item => {
-        boards.innerHTML += `
-            <li id="headerBoard">
-                <p id="headerBoardDescr">
-                    ${item.tag}
-                </p>
-            </li>
-        `;
+    // show menu
+    arrBoards.map(el => {
+        const headerBoard = createElemWithID("li", "headerBoard");
+        const headerBoardDescr = createElemWithID("p", "headerBoardDescr");
+
+        boards.append(headerBoard);
+        headerBoard.append(headerBoardDescr);
+        headerBoardDescr.append(el.tag);
     });
 });
 
-// src
-const URL = "https://63fd39438ef914c555a6fbb7.mockapi.io/api/v1/data";
-
-// get data from URL (promise func)
-const getData = () => {
-    return new Promise((resolve, reject) => {
-        fetch(URL).then(response => response.ok ? resolve(response.json()) : reject(alert("404")))
-    });
+// get data from URL (source)
+const getData = async() => {
+    const data = await fetch(URL).then((res) => res.json());
+    localStorage.setItem("data", JSON.stringify(data));
+    return allData = JSON.parse(localStorage.getItem("data"));
 };
 
-// get data from src
-async function dataToStorage() {
-    const getData = await fetch(URL);
-    const jsonData = await getData.json();
-    // to LS
-    localStorage.removeItem("data");
-    localStorage.setItem("data", JSON.stringify(jsonData));
-};
+// show content and data (init)
+getData().then(init => init.map(content));
 
 // load content (build DOM)
 function content({img, avatar, descr, name, tag, id}) {
-    const mainListItem = createElemWithID("li", id);
-    const mainImgWrap = createElemWithID("div", "mainImgWrap");
-    const mainImg = createElemWithID("img", "mainImg");
+    const mainListItem = createElemWithClass("li", "mainListItem");
+    const mainImgWrap = createElemWithClass("div", "mainImgWrap");
+    const mainImg = createElemWithClass("img", "mainImg");
     const mainInfo = createElemWithID("div", "mainInfo");
     const mainUserInfo = createElemWithID("div", "mainUserInfo");
     const mainAvaWrap = createElemWithID("div", "mainAvaWrap");
@@ -119,17 +59,18 @@ function content({img, avatar, descr, name, tag, id}) {
     const mainDescr = createElemWithID("p", "mainDescr");
     const mainName = createElemWithID("p", "mainName", name);
     const mainTag = createElemWithID("p", "mainTag");
-    const submenu  = createElemWithID("div", "submenu");
+    const submenu  = createElemWithClass("div", "submenu");
+
     const submenuBtnBoard = createElemWithID("button", "submenuBtnBoard", "Add to board");
     const submenuBtnReport = createElemWithID("button", "submenuBtnReport", "Report");
     
     mainImg.setAttribute("src", img);
     mainImg.setAttribute("alt", "img");
+    mainImg.setAttribute("id", id);
     mainAva.setAttribute("src", avatar);
     mainAva.setAttribute("alt", "ava");
     submenuBtnBoard.setAttribute("type", "button");
     submenuBtnReport.setAttribute("type", "button");
-    mainListItem.setAttribute("class", "mainListItem")
 
     mainList.append(mainListItem);
     mainListItem.append(mainImgWrap, mainInfo);
@@ -145,6 +86,7 @@ function content({img, avatar, descr, name, tag, id}) {
     mainImgWrap.addEventListener("mouseover", () => submenu.style.display = "flex");
     // hide submenu
     mainImgWrap.addEventListener("mouseout", () => submenu.style.display = "none");
+
     // show modal-board
     submenuBtnBoard.addEventListener("click", () => {
         toggleOverlay();
@@ -155,66 +97,27 @@ function content({img, avatar, descr, name, tag, id}) {
         toggleOverlay();
         modalReport();
     });
-
-    // get pin-target
-    mainListItem.addEventListener("click", (el) => {
-        let idClickTarget = el.currentTarget.id;
-        let idPinTarget = allData.find((elem => elem.id === idClickTarget));
-        console.log(idPinTarget);
-
-        // overlay btn action
-        overlay.addEventListener("click", (btn) => {
-            let idBtnTarget = btn.target.id;
-            console.log(idBtnTarget);
-            if (idBtnTarget == 1) {
-                let dataBoardOne = JSON.parse(localStorage.getItem(boardOne));
-                boardOne.push(dataBoardOne);
-                localStorage.removeItem("boardOne");
-                boardOne.push(idPinTarget);
-                localStorage.setItem("boardOne", JSON.stringify(boardOne));
-            } 
-            // else if (idBtnTarget == 2) {
-            //     boardTwo.push(idPinTarget);
-            //     let dataBoardTwo = JSON.parse(localStorage.getItem(boardTwo));
-            //     boardTwo.push(dataBoardTwo);
-            //     localStorage.removeItem("boardTwo");
-            //     localStorage.setItem("boardTwo", JSON.stringify(boardTwo));
-            // } else if (idBtnTarget == 3) {
-            //     boardThree.push(idPinTarget);
-            //     let dataBoardThree = JSON.parse(localStorage.getItem(boardThree));
-            //     boardThree.push(dataBoardThree);
-            //     localStorage.removeItem("boardThree");
-            //     localStorage.setItem("boardThree", JSON.stringify(boardThree));
-            // } 
-        });
-    });
-
 };
-    
-// load content and data (init)
-getData().then(init => init.map(content));
-dataToStorage();
 
 // create modal-board
 function modalBoard() {
-    const modal = document.getElementById("modalWindow");
     const modalBoardTitle = createElemWithID("h2", "modalBoardTitle", "Select board:");
     const modalBoardsList = createElemWithID("ul", "modalBoardsList");
     const modalBoardBtnClose = createElemWithID("button", "modalBoardBtnClose", "Close");
 
     modalBoardBtnClose.setAttribute("type", "button");
 
-    modal.innerHTML = "";
-    modal.append(modalBoardTitle, modalBoardsList, modalBoardBtnClose);
+    overlayWindow.innerHTML = "";
+    overlayWindow.append(modalBoardTitle, modalBoardsList, modalBoardBtnClose);
 
-    arrBoards.forEach(item => {
-        modalBoardsList.innerHTML += `
-            <li id="modalBoard">
-                <p id="${item.id}" class="modalBoardDescr">
-                    ${item.tag}
-                </p>
-            </li>
-        `;
+    arrBoards.map(el => {
+        const modalBoardListItem = createElemWithID("li", "modalBoard");
+        const modalBoardListItemDescr = createElemWithID("p", el.id, el.tag);
+
+        modalBoardListItemDescr.setAttribute("class", "modalBoardDescr");
+
+        modalBoardsList.append(modalBoardListItem);
+        modalBoardListItem.append(modalBoardListItemDescr);
     });
 
     // close modal-boards
@@ -223,7 +126,6 @@ function modalBoard() {
 
 // create modal-report
 function modalReport() {
-    const modal = document.getElementById("modalWindow");
     const modalReportTitle = createElemWithID("h2", "modalReportTitle", "Select problem(s):");
     const modalReportList = createElemWithID("ul", "modalReportList");
     const modalReportBtnWrap = createElemWithID("div", "modalReportBtnWrap");
@@ -231,89 +133,108 @@ function modalReport() {
     const modalReportBtnSend = createElemWithID("button", "modalReportBtnSend", "Send");
 
     modalReportBtnClose.setAttribute("type", "button");
-    modalReportBtnSend.setAttribute("type", "submit");
+    modalReportBtnSend.setAttribute("type", "button");
 
-    modal.innerHTML = "";
-    modal.append(modalReportTitle, modalReportList, modalReportBtnWrap);
+    overlayWindow.innerHTML = "";
+    overlayWindow.append(modalReportTitle, modalReportList, modalReportBtnWrap);
     modalReportBtnWrap.append(modalReportBtnSend, modalReportBtnClose);
 
-    arrProblems.forEach((item) => {
-        modalReportList.innerHTML += `
-            <li id="modalReportListItem">
-                <input id="modalReportCheckbox" type="checkbox"/> 
-                <p id="modalReportListItemDescr">
-                    ${item.tag}
-                </p>
-            </li>
-        `;
+    arrProblems.map(el => {
+        const modalReportListItem = createElemWithID("li", "modalReportListItem");
+        const modalReportCheckbox = createElemWithID("input", "modalReportCheckbox");
+        const modalReportListItemDescr = createElemWithID("p", "modalReportListItemDescr");
+
+        modalReportCheckbox.setAttribute("type", "checkbox");
+
+        modalReportList.append(modalReportListItem);
+        modalReportListItem.append(modalReportCheckbox, modalReportListItemDescr);
+        modalReportListItemDescr.append(el.tag);
     });
 
     // close modal-report
     modalReportBtnClose.addEventListener("click", () => toggleOverlay());
 };
 
-// toggle overlay
-function toggleOverlay() {
-    const overlay = document.getElementById("overlay");
-    overlay.style.display == "flex" ? overlay.style.display = "none" : overlay.style.display = "flex";
-};
-
-// search
-const search = () => {
-    for (let i = 0; i < allData.length; i++) {
-        let elem = allData[i];
-        if (input.value === elem.tag) {
-            mainList.innerHTML = ""
-            mainList.innerHTML += `
-                <li class="mainListItem" id="${elem.id}">
-                    <div id="mainImgWrap">
-                        <img id="mainImg" src="${elem.img}" alt="img">
-                        <div id="submenu">
-                            <button id="submenuBtnBoard" type="button">
-                                Add to board
-                            </button>
-                            <button id="submenuBtnReport" type="button">
-                                Report
-                            </button>
-                        </div>
-                    </div>
-                    <div id="mainInfo">
-                        <p id="mainTag">#${elem.tag}</p>
-                        <div id="mainUserInfo">
-                            <div id="mainAvaWrap">
-                                <img id="mainAva" src="${elem.avatar}" alt="ava">
-                            </div>
-                            <p id="mainName">${elem.name}</p>
-                        </div>
-                        <p id="mainDescr">${elem.descr}</p>
-                    </div>
-                </li>
-            `;
-        };
-    };
-    // show submenu
-    mainImgWrap.addEventListener("mouseover", () => submenu.style.display = "flex");
-    // hide submenu
-    mainImgWrap.addEventListener("mouseout", () => submenu.style.display = "none");
-    // show modal-board
-    submenuBtnBoard.addEventListener("click", () => {
-        toggleOverlay();
-        modalBoard();
-    });
-    // show modal-report
-    submenuBtnReport.addEventListener("click", () => {
-        toggleOverlay();
-        modalReport();
-    });
-};
-
 // search run
 btnSearch.addEventListener("click", () => {
-    (input.value != "") ? search() : null;
+    if (input.value != "") {
+        search();
+    };
+
     input.value = "";
     input.focus();
 });
 
+// search
+const search = () => {
+    // get arr with pin-target
+    let pinTarget = allData.filter(el => el.tag === input.value);
+    // clear ul
+    mainList.innerHTML = "";
+    // show pin-target(s)
+    pinTarget.map(el => content(el));
+};
 
+// toggle overlay
+function toggleOverlay() {
+    const overlayWrap = document.getElementById("overlay");
+    overlayWrap.style.display == "flex" ? overlayWrap.style.display = "none" : overlayWrap.style.display = "flex";
+};
 
+// catch pin from data
+mainList.addEventListener("mouseover", el => {
+    let idTarget = el.target.id;
+    let pin = allData.find(el => el.id === idTarget);
 
+    if (pin != undefined) {
+        localStorage.setItem("temp", JSON.stringify(pin));
+    };
+});
+
+// collect pins to boards (local storage)
+overlayWindow.addEventListener("click", el => {
+    // get pin-target
+    let idTarget = el.target.id;
+    let pin = JSON.parse(localStorage.getItem("temp"));
+    // collect to boards
+    if (idTarget === "brd1") {
+    localStorage.setItem("board-1", JSON.stringify([...JSON.parse(localStorage.getItem("board-1") || "[]"), pin]));
+    } else if (idTarget === "brd2") {
+    localStorage.setItem("board-2", JSON.stringify([...JSON.parse(localStorage.getItem("board-2") || "[]"), pin]));
+    } else if (idTarget === "brd3") {
+    localStorage.setItem("board-3", JSON.stringify([...JSON.parse(localStorage.getItem("board-3") || "[]"), pin]));
+    };
+
+    // hide pin
+    if (idTarget === "modalReportBtnSend") {
+        filterData = allData.filter(el => el.id != pin.id);
+        // change data (src)
+        localStorage.setItem("data", JSON.stringify(filterData));
+        // new arr-data
+        allData = JSON.parse(localStorage.getItem("data"));
+        // clear ul
+        mainList.innerHTML = "";
+        // show new content
+        allData.map(el => content(el));
+        // close modal window
+        toggleOverlay();
+    };
+});
+
+// show collection of board
+boards.addEventListener("click", el => {
+    let brdName = el.target.innerHTML;
+    let getDataBrd1 = JSON.parse(localStorage.getItem("board-1"));
+    let getDataBrd2 = JSON.parse(localStorage.getItem("board-2"));
+    let getDataBrd3 = JSON.parse(localStorage.getItem("board-3"));
+
+    mainList.innerHTML = "";
+    
+    if (brdName === "Board-1") {
+        getDataBrd1.map(el => content(el));
+    } else if (brdName === "Board-2") {
+        getDataBrd2.map(el => content(el));
+    } else if (brdName === "Board-3") {
+        getDataBrd3.map(el => content(el));
+    };
+});
